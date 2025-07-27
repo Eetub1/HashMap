2fd233ac2cee5172187747ef4588f50e52e87336
Eetu <eetu.oinonen04@gmail.com>
2025-07-27 15:55:00 +0300

    hashmap is about ready

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ class HashMap {
         this.loadFactor = loadFactor
         this.capacity = capacity
         this.currentLoad = 0
+        this.keysCount = 0
         this.buckets = []
         this.buckets.length = capacity
+        this.originalCapacity = capacity
     }
 
     growBuckets() {
@@ -32,7 +34,7 @@ class HashMap {
 
     set(key, value) {
         const index = this.hash(key)
-        console.log(index)
+        //console.log(index)
         if (index < 0 || index >= this.buckets.length) {
             throw new Error("Trying to access index out of bounds");
         }
@@ -40,12 +42,12 @@ class HashMap {
             const list = new LinkedList()
             list.append({key, value})
             this.buckets[index] = list
-            this.currentLoad += 1
+            this.currentLoad++
+            this.keysCount++
         } else {
             const list = this.buckets[index]
             const containsKey = list.contains(key)
             if (containsKey) {
-                console.log("Täällä pitäisi vaihtaa arvo")
                 let current = list.head()
                 while (current) {
                     if (current.value.key === key) {
@@ -54,8 +56,8 @@ class HashMap {
                     }
                 }
             } else {
-                console.log("Täällä pitäisi lisätä arvo listan loppuun")
                 list.append({key, value})
+                this.keysCount++
             }
         }
         if (this.currentLoad / this.capacity >= this.loadFactor) {
@@ -69,7 +71,14 @@ class HashMap {
             throw new Error("Trying to access index out of bounds");
         }
         if (this.buckets[index] === undefined) return null
-        return this.buckets[index]
+        const list = this.buckets[index]
+        //voisiko tässä käyttää linkedlist luokassa olevaa valmista metodia?
+        let current = list.head()
+        while (current) {
+            if (current.value.key === key) return current.value.value
+            current = current.nextNode
+        }
+        return null
     }
 
     has(key) {
@@ -78,7 +87,88 @@ class HashMap {
             throw new Error("Trying to access index out of bounds");
         }
         if (this.buckets[index] === undefined) return false
-        return true
+        const list = this.buckets[index]
+        //voisiko tässä käyttää linkedlist luokassa olevaa valmista metodia?
+        let current = list.head()
+        while (current) {
+            if (current.value.key === key) return true
+            current = current.nextNode
+        }
+        return false
+    }
+
+    remove(key) {
+        const index = this.hash(key)
+        if (index < 0 || index >= this.buckets.length) {
+            throw new Error("Trying to access index out of bounds");
+        }
+        if (this.buckets[index] === undefined) return false
+        const list = this.buckets[index]
+        //täälläkin vois hyödyntää valmiita metodeja? tai ehkä ei! muahahahah
+        let current = list.head()
+        let i = 0
+        while (current) {
+            if (current.value.key === key) {
+                list.removeAt(i)
+                if (list.size() === 1) this.currentLoad--
+                this.keysCount--
+                return true
+            }
+            current = current.nextNode
+            i++
+        }
+        return false
+    }
+
+    length() {return this.keysCount}
+
+    clear() {
+        this.capacity = this.originalCapacity
+        this.buckets = []
+        this.currentLoad = 0
+        this.keysCount = 0
+    }
+
+    keys() {
+        const keys = []
+        for (let i = 0; i < this.buckets.length; i++) {
+            if (this.buckets[i] === undefined) continue
+            const list = this.buckets[i]
+            let current = list.head()
+            while (current) {
+                keys.push(current.value.key)
+                current = current.nextNode
+            }
+        }
+        return keys
+    }
+
+    values() {
+        const values = []
+        for (let i = 0; i < this.buckets.length; i++) {
+            if (this.buckets[i] === undefined) continue
+            const list = this.buckets[i]
+            let current = list.head()
+            while (current) {
+                values.push(current.value.value)
+                current = current.nextNode
+            }
+        }
+        return values
+    }
+
+    entries() {
+        const entries = []
+        for (let i = 0; i < this.buckets.length; i++) {
+            if (this.buckets[i] === undefined) continue
+            const list = this.buckets[i]
+            let current = list.head()
+            while (current) {
+                entries.push([current.value.key, current.value.value])
+                current = current.nextNode
+            }
+        }
+        return entries
     }
 
     show() {
@@ -105,20 +195,42 @@ function main() {
     test.set('carrot', 'blorange')
     console.log("Load: ", test.currentLoad)
     console.log("Current length: ", test.buckets.length)
+    console.log("Is there an apple? ", test.has("apple"))
     console.log()
 
     console.log("Every item: ")
     test.show()
+    console.log("Avainten määrä: ", test.length())
     console.log()
+
+    console.log(test.remove("kite"))
+    test.show()
+    console.log("Avainten määrä: ", test.length())
+    console.log()
+
+    test.set('kite', 'pink')
+    test.show()
+    console.log("Avainten määrä: ", test.length())
+
+    console.log("Avaimet: ", test.keys())
+    console.log("Arvot: ", test.values())
+    console.log("Avaimet ja arvot: ", test.entries())
+
+    /*test.clear()
+    console.log("Load: ", test.currentLoad)
+    console.log("Current length: ", test.buckets.length)
+    test.show()*/
+
 
     /*test.set("jasatekko", "jotain")
     test.set("eethuy", "juuso")
     console.log("Load: ", test.currentLoad)
     console.log("Current length: ", test.buckets.length)
-    console.log()*/
+    console.log()
 
+    console.log(test.get("carrot"))
     console.log(test.get("apple"))
-    console.log(test.has("apple"))
+    console.log("Is there an apple? ", test.has("apple"))*/
 
 }
 
