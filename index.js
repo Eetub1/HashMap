bdc35b11648ec44a5ea4fc733861a936de7e8c44
Eetu <eetu.oinonen04@gmail.com>
2025-07-27 14:17:32 +0300

    the set method is almost ready

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+const { LinkedList } = require('./linkedlist.js')
+
 class HashMap {
     constructor(loadFactor, capacity) {
         this.loadFactor = loadFactor
         this.capacity = capacity
+        this.currentLoad = 0
+        this.buckets = []
+        this.buckets.length = capacity
+    }
+
+    growBuckets() {
+        const newArr = []
+        newArr.length = this.capacity * 2
+        for (let i = 0; i < this.capacity; i++) {
+            if (this.buckets[i] !== undefined) {
+                newArr[i] = this.buckets[i]
+            }
+        }
+        this.buckets = newArr
+        this.capacity *= 2
     }
 
     hash(key) {
@@ -10,13 +27,92 @@ class HashMap {
         for (let i = 0; i < key.length; i++) {
         hashCode = primeNumber * hashCode + key.charCodeAt(i);
         }
-        return hashCode;
+        return hashCode % this.buckets.length;
+    }
+
+    set(key, value) {
+        const index = this.hash(key)
+        console.log(index)
+        if (index < 0 || index >= this.buckets.length) {
+            throw new Error("Trying to access index out of bounds");
+        }
+        if (this.buckets[index] === undefined) {
+            const list = new LinkedList()
+            list.append({key, value})
+            this.buckets[index] = list
+            this.currentLoad += 1
+        } else {
+            const list = this.buckets[index]
+            const containsKey = list.contains(key)
+            if (containsKey) {
+                console.log("Täällä pitäisi vaihtaa arvo")
+            } else {
+                console.log("Täällä pitäisi lisätä arvo listan loppuun")
+            }
+        }
+
+        if (this.currentLoad / this.capacity >= this.loadFactor) {
+            this.growBuckets()
+        }
+    }
+
+    get(key) {
+        const index = this.hash(key)
+        if (index < 0 || index >= this.buckets.length) {
+            throw new Error("Trying to access index out of bounds");
+        }
+        if (this.buckets[index] === undefined) return null
+        return this.buckets[index]
+    }
+
+    has(key) {
+        const index = this.hash(key)
+        if (index < 0 || index >= this.buckets.length) {
+            throw new Error("Trying to access index out of bounds");
+        }
+        if (this.buckets[index] === undefined) return false
+        return true
+    }
+
+    show() {
+        for (const bucket of this.buckets) {
+            console.log(bucket)
+        }
     }
 }
 
 function main() {
-    const map = new HashMap(0.7, 16)
-    console.log(map.hash("eetv"))
+    const test = new HashMap(0.75, 16)
+    test.set('apple', 'red')
+    test.set('banana', 'yellow')
+    test.set('carrot', 'orange')
+    test.set('dog', 'brown')
+    test.set('elephant', 'gray')
+    test.set('frog', 'green')
+    test.set('grape', 'purple') //11
+    test.set('hat', 'black') //11 same hashcode
+    test.set('ice cream', 'white')
+    test.set('jacket', 'blue')
+    test.set('kite', 'pink')
+    test.set('lion', 'golden')
+    test.set('carrot', 'blorange')
+    console.log("Load: ", test.currentLoad)
+    console.log("Current length: ", test.buckets.length)
+    console.log()
+
+    console.log("Every item: ")
+    test.show()
+    console.log()
+
+    /*test.set("jasatekko", "jotain")
+    test.set("eethuy", "juuso")
+    console.log("Load: ", test.currentLoad)
+    console.log("Current length: ", test.buckets.length)
+    console.log()*/
+
+    console.log(test.get("apple"))
+    console.log(test.has("apple"))
+
 }
 
 main()
---
+++ linkedlist.js
@@ -0,0 +1,148 @@
+class LinkedList {
+    constructor() {
+        this.firstNode = null
+        this.lastNode = null
+        this.length = 0
+    }
+
+    append(value) {
+        const newNode = new Node(value)
+        if (this.firstNode === null) {
+            this.firstNode = newNode
+        } else {
+            this.lastNode.nextNode = newNode
+        }
+        this.lastNode = newNode
+        this.length++
+    }
+
+    prepend(value) {
+        const newNode = new Node(value)
+        if (this.firstNode === null) {
+            this.firstNode = newNode
+            this.lastNode = newNode
+        } else {
+            const currentFirstNode = this.firstNode
+            this.firstNode = newNode
+            this.firstNode.nextNode = currentFirstNode
+        }
+        this.length++
+    }
+
+    size() {return this.length}
+
+    head() {return this.firstNode}
+
+    tail() {return this.lastNode}
+
+    at(index) {
+        let size = this.size()
+        if (index >= size || index < 0) return null
+        let current = this.head()
+        for (let i = 0; i < index; i++) {
+            current = current.nextNode 
+        }
+        return current
+    }
+
+    pop() {
+        const size = this.size()
+        if (size < 1) return
+        if (size < 2) {
+            this.firstNode = null
+            this.lastNode = null
+        } else {
+            const secondLast = this.at(size - 2)
+            secondLast.nextNode = null
+            this.lastNode = secondLast
+        }
+        this.length--
+    }
+
+    contains(key) {
+        let current = this.head()
+        while (current) {
+            if (current.value.key === key) return true
+            current = current.nextNode
+        }
+        return false
+    }
+
+    find(value) {
+        const size = this.size()
+        if (size < 1) return null
+        let current = this.head()
+        for (let i = 0; i < size; i++) {
+            if (current.value === value) return i
+            current = current.nextNode
+        }
+        return null
+    }
+
+    toString() {
+        const size = this.size()
+        if (size < 1) return "( null )"
+        let listAsString = ""
+        let current = this.head()
+        while (current) {
+            listAsString += `( ${current.value} ) -> `
+            current = current.nextNode
+        }
+        listAsString += "null"
+        return listAsString
+    }
+
+    insertAt(value, index) {
+        const size = this.size()
+        if (index < 0 || index > size) return
+        const newNode = new Node(value)
+        if (size === 0 && index === 0) {
+            this.firstNode = newNode
+            this.lastNode = newNode
+        //here it is guaranteed that size > 0
+        } else if (index === size) {
+            const currentLast = this.lastNode
+            this.lastNode = newNode
+            currentLast.nextNode = newNode
+        } else if (index === 0) {
+            const currentFirst = this.head()
+            this.firstNode = newNode
+            newNode.nextNode = currentFirst
+        } else {
+            const prev = this.at(index - 1)
+            newNode.nextNode = prev.nextNode
+            prev.nextNode = newNode
+        }
+        this.length++
+    }
+
+    removeAt(index) {
+        const size = this.size()
+        if (index >= size || index < 0) return
+        if (size === 1) {
+            this.firstNode = null
+            this.lastNode = null
+        } else if (index === size - 1) {
+            const prev = this.at(index - 1)
+            prev.nextNode = null
+            this.lastNode = prev
+        } else if (index === 0) {
+            this.firstNode = this.firstNode.nextNode
+        } else {
+            const prev = this.at(index - 1)
+            const next = prev.nextNode.nextNode
+            prev.nextNode = next
+        }
+        this.length--
+    }
+}
+
+class Node {
+    constructor(value=null, nextNode=null) {
+        this.value = value
+        this.nextNode = nextNode
+    }
+}
+
+module.exports = { LinkedList }
+
